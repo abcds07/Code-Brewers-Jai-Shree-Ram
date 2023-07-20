78fdc8a11773609fa3ba61675710ae46863c3d26
Gyanu Mayank <81113356+gyanu2507@users.noreply.github.com>
2023-07-21 01:48:06 +0530

    Add files via upload

---
+++ delete_rarely_accessed_files.py
@@ -0,0 +1,10 @@
+import os
+
+def delete_rarely_accessed_files(file_paths):
+    for file_path in file_paths:
+        try:
+            # Delete the file
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
+        except OSError as e:
+            print(f"Error deleting {file_path}: {e}")
